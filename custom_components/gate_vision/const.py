8e7aae5abd93c84ff7886019b15cda5aee567c90
Bezuglyy E.N <78296526+bezuglyy@users.noreply.github.com>
2026-09-18 10:11:09 +1000

    v1.6.1: custom_components/gate_vision/const.py

--- custom_components/gate_vision/const.py
+++ custom_components/gate_vision/const.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 DOMAIN = "gate_vision"
 NAME = "Обнаружение"
 MANUFACTURER = "techlan.su"
-VERSION = "1.0.0"
+VERSION = "1.6.1"
 
 # --- ключи настроек (config entry data/options) ---
 CONF_CAMERA_ENTITY = "camera_entity"
